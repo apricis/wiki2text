76740759813083369314a551bb5ce9be897ffa73
Dmytro Kalpakchi <dkalpackchi@gmail.com>
2017-11-21 19:22:58 +0100

    Loading wikipedia templates and modules from templates XML file

--- wiki2text.nim
+++ wiki2text.nim
@@ -7,7 +7,7 @@
 # through another streaming XML parser. This one doesn't really need to be
 # streaming, but it might as well be so that we can reuse the same Nim library.
 
-import streams, parsexml, re, strutils, unicode, parseopt2
+import streams, parsexml, re, strutils, unicode, parseopt2, tables
 
 # Wikitext handling
 # -----------------
@@ -30,6 +30,7 @@ const SKIP_SPANS = [
 #   ^#\s*(REDIRECT|redirect).*$  = Redirect syntax
 #   ^[ *#:;]+                    = Bullets and indentation markers at the start of a line
 #   ^__.*__$                     = table-of-contents directives
+#   __.*__                       = Magic words behavioural switches (generic table-of-contents directives)
 #   ^[|!].*$                     = Table detritus
 #
 # "Table detritus" might require some explanation. Tables, delimited by {|
@@ -43,7 +44,7 @@ const SKIP_SPANS = [
 # though, these lines begin with the cell separator |, so we can just filter
 # those out.
 
-let FORMATTING_RE: Regex = re(r"('''?|^#\s*redirect.*$|^[ *#:;]+|^[|!].*$|^__.*__$)", {reMultiLine, reIgnoreCase})
+let FORMATTING_RE: Regex = re(r"('''?|^#\s*redirect.*$|^[ *#:;]+|^[|!].*$|__.*__)", {reMultiLine, reIgnoreCase})
 
 # This regex matches sequences of more than one blank line.
 let BLANK_LINE_RE: Regex = re"\n\s*\n\s*\n"
@@ -53,6 +54,27 @@ let WORD_SEPARATOR_RE: Regex = re"'?([\x01-\x26\x28-\x2f\x3a-\x40\x5b-\x60\x7b-\
 let EMPTY_REF_RE: Regex = re(r"<ref [^>]+/\s*>", {reIgnoreCase})
 
 const FAKE_FILENAME = "<wikipage>"
+
+# Simple templates handling
+let SUBST_WORDS_RE = re("(subst:|safesubst:)", {reIgnoreCase})
+let COMMENT_OR_NOINCLUDE_RE = re(r"(<!--.*?-->|<noinclude>(?:.*?)</noinclude>|<noinclude\s*>.*$)", {reDotAll})
+let UNTERMINATED_NOINCLUDE_RE = r"<noinclude/>"
+let INCLUDE_ONLY_RE = re(r"<includeonly>|</includeonly>", {reDotAll})
+let ONLY_INCLUDE_RE = re(r"<onlyinclude>(.*?)</onlyinclude>", {reDotAll})
+let NAMED_TEMPLATE_PARAM_RE = r" *([^=]*?) *?=(.*)"
+
+# Magic Words handling
+# see https://doc.wikimedia.org/mediawiki-core/master/php/MagicWord_8php_source.html
+# const MAGIC_WORDS_SWITCHES = [
+#     "__NOTOC__", "__FORCETOC__", "__TOC__", "__NEWSECTIONLINK__", "__NONEWSECTIONLINK__", "__NOGALLERY__",
+#     "__HIDDENCAT__", "__NOCONTENTCONVERT__", "__NOCC__", "__NOTITLECONVERT__", "__NOTC__", "__START__",
+#     "__END__", "__INDEX__", "__NOINDEX__", "__STATICREDIRECT__", "__DISAMBIG__", "__NOEDITSECTION__"
+# ]
+# let MAGIC_WORDS_RE = re(r"($#)" % MAGIC_WORDS_SWITCHES.join("|"))
+const MAGIC_WORDS_TEMPLATES = [
+    "NAMESPACE", "NAMESPACENUMBER", "PAGENAME", "FULLPAGENAME", "BASEPAGENAME", "SUBPAGENAME",
+    "ROOTPAGENAME", "CURRENTYEAR", "CURRENTMONTH", "CURRENTDAY", "CURRENTHOUR", "CURRENTTIME"
+]
 
 proc skipNestedChars(text: string, pos: var int, open: char, close: char) =
     ## Move our position 'pos' forward in the text, to skip a number of
@@ -85,9 +107,9 @@ proc unicodeLower(text: string): string =
     for rune in runes(text):
         result.add(rune.toLower.toUTF8)
 
-
 # forward declaration
-proc filterWikitext(text: string): string
+proc filterWikitext(text: string, templates: TableRef[string, TableRef[string, string]] = nil): string
+
 
 proc extractInternalLink(linkText: string): string =
     # Links with colons might be special MediaWiki syntax. Just throw them
@@ -165,8 +187,55 @@ proc filterHTML(origText: string): string =
     # return result implicitly
     xml.close
 
+# proc parseTemplateParams(params: array):
 
-proc filterWikitext(text: string): string =
+proc expandSimpleTemplate(text: string, pos: var int, templates: TableRef[string, TableRef[string, string]] = nil): string =
+    ## Given a complete wikitext and position where "{{" was found
+    ## try to expand simple non-recursive templates aka {{ | | | | }}
+    ## If recursive template was found -- simply return false
+    let 
+        templateBeginPos = pos
+        open = '{'
+        openBracket = '['
+        close = '}'
+    pos += 2
+    var count = 2
+    while count > 0 and pos < text.len:
+        let nextPos: int = text.find({open, openBracket, close}, pos)
+        if nextPos == -1:
+            # We can't find any more closing characters in the text.
+            # Jump to the end and abort.
+            pos = text.len
+            return
+        else:
+            let nextChar: char = text[nextPos]
+            if nextChar == open or nextChar == openBracket:
+                # means nested templates -- skipping
+                # or templates with links inside them
+                pos -= 2
+                return nil
+            else:
+                count -= 1
+            pos = nextPos + 1
+    
+    # this is the simple template text inside {{ }}
+    echo "TEMPLATE: " & text[templateBeginPos+2..<pos-2]
+    let templateParts = text[templateBeginPos+2..<pos-2].split("|")
+    echo "TEMPLATE PARTS: " & templateParts
+    var title = templateParts[0].strip()
+    echo "TITLE: " & title
+
+    let endSubst = title.matchLen(SUBST_WORDS_RE)
+    if endSubst != -1:
+        title = title.replace(title[0..endSubst])
+    if title in MAGIC_WORDS_TEMPLATES or title.find(':') != -1:
+        # we skip magic word templates, since they don't give any useful info for QA
+        # if we encounter ":" in title, it means, we've encountered a parser function
+        # parser functions are not supported for now
+        pos = templateBeginPos - 2
+        return nil
+
+proc filterWikitext(text: string, templates: TableRef[string, TableRef[string, string]] = nil): string =
     ## Given the complete wikitext of an article, filter it for the part
     ## that's meant to be read as plain text.
 
@@ -189,7 +258,13 @@ proc filterWikitext(text: string): string =
         pos = found
         if pos < text.len:
             let next2chars: string = text[pos .. pos+1]
-            if next2chars == "{{" or next2chars == "{|":
+            if next2chars == "{{":
+                if templates != nil:
+                    let expanded: string = expandSimpleTemplate(text, pos, templates)
+                skipNestedChars(text, pos, '{', '}')
+
+            elif next2chars == "{|":
+                # this must be table -- skip
                 skipNestedChars(text, pos, '{', '}')
 
             elif text[pos] == '[':
@@ -218,17 +293,19 @@ type
     TagType = enum
         TITLE, TEXT, REDIRECT, NS
     ArticleData = array[TagType, string]
+    TemplateData = array[TagType, string]
 
 var RELEVANT_XML_TAGS = ["title", "text", "ns"]
+var RELEVANT_TEMPLATE_XML_TAGS = ["title", "text", "ns", "id"]
 
-proc handleArticle(article: ArticleData, tokenize: bool) =
+proc handleArticle(article: ArticleData, tokenize: bool, templates: TableRef[string, TableRef[string, string]] = nil) =
     if article[NS] == "0" and article[REDIRECT] == "":
         if not tokenize:
             echo("= $1 =" % [article[TITLE]])
         # Parse the article inside a try/except block, discarding the errors
         # that appear due to occasional HTML that's flagrantly bad XML.
         try:
-            let text = filterWikitext(filterHTML(article[TEXT]))
+            let text = filterWikitext(filterHTML(article[TEXT]), templates)
             if tokenize:
                 let words = text.split(WORD_SEPARATOR_RE)
                 for word in words:
@@ -241,8 +318,7 @@ proc handleArticle(article: ArticleData, tokenize: bool) =
         except RangeError:
             discard
 
-
-proc readMediaWikiXML(input: Stream, tokenize: bool, filename="<input>") =
+proc readMediaWikiXML(input: Stream, tokenize: bool, filename="<input>", templates: TableRef[string, TableRef[string, string]] = nil) =
     ## Read the XML content that one actually downloads from Wikimedia,
     ## extracting the article content and sending it to the handleArticle()
     ## procedure.
@@ -304,7 +380,7 @@ proc readMediaWikiXML(input: Stream, tokenize: bool, filename="<input>") =
             of "page":
                 # When we reach the end of the <page> tag, send the article
                 # data to handleArticle().
-                handleArticle(article, tokenize)
+                handleArticle(article, tokenize, templates)
             else:
                 discard
 
@@ -319,6 +395,81 @@ proc readMediaWikiXML(input: Stream, tokenize: bool, filename="<input>") =
             discard
     xml.close
 
+proc loadTemplates(input: Stream, filename="<templates>"): TableRef[string, TableRef[string, string]] =
+    var 
+        xml: XmlParser
+        textBuffer: string = ""
+        wikiTemplate: TemplateData
+    result = newTable[string,TableRef[string,string]]()
+    result["templates"] = newTable[string,string]()
+    result["modules"] = newTable[string,string]()
+
+    for tag in TITLE..NS:
+        wikiTemplate[tag] = ""
+
+    # Keep track of what text content represents. Is it article text, or a
+    # similar text property of the page? Is it an attribute value on a tag that
+    # we should pay attention to?
+    var gettingText: bool = false
+
+    xml.open(input, filename, options={reportWhitespace})
+    while true:
+        # Scan through the XML, handling each token as it arrives.
+        xml.next()
+        case xml.kind
+        of xmlElementStart, xmlElementOpen:
+            if RELEVANT_TEMPLATE_XML_TAGS.contains(xml.elementName):
+                # If this is a "title", "text", "ns" or "id" tag, prepare to get its
+                # text content. Move our writing pointer to the beginning of
+                # the text buffer, so we can overwrite what was there.
+                textBuffer.setLen(0)
+                gettingText = true
+        of xmlCharData, xmlWhitespace:
+            # If we're looking for text, and we found it, add it to the buffer.
+            if gettingText:
+                textBuffer.add(xml.charData)
+        of xmlElementEnd:
+            # When we reach the end of an element we care about, take the text
+            # we've found and store it in the 'wikiTemplate' data structure. We can
+            # accomplish this quickly by simply swapping their references.
+            case xml.elementName
+            of "title":
+                swap wikiTemplate[TITLE], textBuffer
+            of "text":
+                swap wikiTemplate[TEXT], textBuffer
+            of "page":
+                # When we reach the end of the <page> tag, send the wikiTemplate
+                # data to handleArticle().
+                let titleParts = wikiTemplate[TITLE].split(':')
+
+                if titleParts[0] == "Template":
+                    # remove comments and proper/unterminated <noinclude> tags
+                    var text = wikiTemplate[TEXT].replace(COMMENT_OR_NOINCLUDE_RE)
+                    text = text.replace(UNTERMINATED_NOINCLUDE_RE)
+
+                    let onlyInclude = text.findAll(ONLY_INCLUDE_RE)
+
+                    if onlyInclude.len > 0:
+                        text = onlyInclude.join("")
+                    else:
+                        text = text.replace(INCLUDE_ONLY_RE)
+
+                    if text != "":
+                        result["templates"][unicode.toLower(titleParts[1])] = text
+                elif titleParts[0] == "Module":
+                    result["modules"][unicode.toLower(titleParts[1])] = wikiTemplate[TEXT]
+            else:
+                discard
+
+            # Now that we've reached the end of an element, stop extracting
+            # text. (We'll never need to extract text from elements that can
+            # have other XML elements nested inside them.)
+            gettingText = false
+        of xmlEof:
+            break
+        else:
+            discard
+    xml.close
 
 const helptext: string = """
 wiki2text - transform MediaWiki XML to text
@@ -337,20 +488,27 @@ when isMainModule:
     var
         tokenize: bool = false
         run: bool = true
+        templateFile: string = ""
     for kind, key, val in getopt():
         case kind
         of cmdLongOption, cmdShortOption:
             case key
-            of "tokenize", "t":
+            of "tokenize", "tk":
                 tokenize = true
             of "help", "h":
                 writeHelp()
                 run = false
+            of "templates", "t":
+                templateFile = val
             else:
                 discard
         else:
             discard
 
     if run:
+        if templateFile != "":
+            var templates: TableRef[string,TableRef[string,string]] = loadTemplates(newFileStream(open(templateFile)))
+            echo(templates["templates"]["convert"])
+            echo(templates["modules"]["convert"])
         readMediaWikiXML(newFileStream(stdin), tokenize)
 
